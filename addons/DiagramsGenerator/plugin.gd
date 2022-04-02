a359a29463d63f7f1b63df26226a19c3252e7683
arsany <arslino@hotmail.com>
2022-04-02 21:38:13 +0200

    Complet parsing and cleaning the noes/signals info

--- addons/DiagramsGenerator/plugin.gd
+++ addons/DiagramsGenerator/plugin.gd
@@ -1,29 +1,25 @@
 tool
 extends EditorPlugin
 
+var parent_of_nodes_in_file = ""
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
-	print("_enter_tree!")
 	# Load the  doc_dock scene and instance it.
 	var doc_dock = load("res://addons/DiagramsGenerator/Doc_Dock_Control.tscn").instance()
 	doc_dock.get_node("Doc_Dock_Button").connect("pressed",self,"_on_button_pressed")
 	add_child(doc_dock)
-	
-	
+
+
 func _on_button_pressed():
+	#TODO --- link all nodes !!!!!!
 	var parsed_files = Collect_ModelFiles("res://")
 	Parse_ModelFiles(parsed_files)
-	
-	#TODO --- link all nodes !!!!!!
-						
-	pass
+
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
-	# Remove the doc_dock.
-	print("_exit_tree!")
-
+	pass
 
 
 func Collect_ModelFiles(scan_dir : String) -> Array:
@@ -51,12 +47,12 @@ func Collect_ModelFiles(scan_dir : String) -> Array:
 	return my_files
 
 
-
 func Parse_ModelFiles(parsed_files):
 	var parsed_lines = {"Nodes": [],"Signals": []}
 	for file_name in parsed_files:
 		var parsed_file = File.new()
 		var valid_nodes_info = []
+		var valid_signals_info = []
 				
 		parsed_file.open(file_name, File.READ)
 			
@@ -64,35 +60,23 @@ func Parse_ModelFiles(parsed_files):
 			var line = parsed_file.get_line()
 
 			if line.begins_with("[node name="):
-				var line_info
-				line_info = Get_LineInfo(line,"Node")
+				var line_info = Get_LineInfo(line,"Node")
 				if line_info:
-
 					valid_nodes_info.append(line_info)
 
 			elif line.begins_with("[connection signal="):
-				#parsed_lines["Signals"].append(line)
-				# TODO ---- handle signals in get info !!!!!!
-				pass
+				var line_info = Get_LineInfo(line,"Signal")
+				if line_info:
+					valid_signals_info.append(line_info)
 
-		parsed_lines["Nodes"].append_array(Clean_NodeInfo(valid_nodes_info))
+		# Node shall be handled before signals, such that the parent_of_nodes_in_file will get a value
+		parsed_lines["Nodes"].append_array(Clean_Info(valid_nodes_info,"Node"))
+		parsed_lines["Signals"].append_array(Clean_Info(valid_signals_info,"Signal"))
+		parent_of_nodes_in_file = ""
 		parsed_file.close()
 		
-	#print(parsed_lines)
-		
-func Clean_NodeInfo(nodes_info : Array) -> Array:
-	
-	var parent_of_nodes = ""
-	
-	for node in nodes_info:
-		if node["parent"] == "":
-			parent_of_nodes = node["name"]
-		
-		elif node["parent"] == "." and parent_of_nodes != "":
-			node["parent"] = parent_of_nodes
-		
-	
-	return nodes_info
+	print(parsed_lines)
+
 
 func Get_LineInfo(line : String, type : String) -> Dictionary:
 
@@ -105,7 +89,6 @@ func Get_LineInfo(line : String, type : String) -> Dictionary:
 			node_infos["name"] = result.get_string(1)
 			node_infos["type"] = result.get_string(2)
 			node_infos["parent"] = result.get_string(4)			
-
 			return node_infos
 		
 		else:
@@ -117,7 +100,35 @@ func Get_LineInfo(line : String, type : String) -> Dictionary:
 				node_infos["name"] = result2.get_string(1)
 				node_infos["parent"] = result2.get_string(2)
 				node_infos["type"] = result2.get_string(3)			
-				
 				return node_infos
-
+	
+	elif  type == "Signal":
+		var signal_infos = {"signal":"","from":"","to":"","method":""}
+		var regex = RegEx.new()
+		regex.compile("connection signal=\"(.*?)\" from=\"(.*?)\" to=\"(.*?)\" method=\"(.*?)\"")
+		var result = regex.search(line)
+		if result:
+			signal_infos["signal"] = result.get_string(1)
+			signal_infos["from"] = result.get_string(2)
+			signal_infos["to"] = result.get_string(3)
+			signal_infos["method"] = result.get_string(4)
+			return signal_infos
+		
 	return {}
+
+
+func Clean_Info(info : Array, type : String) -> Array:
+	if type == "Node":
+		for node in info:
+			if node["parent"] == "":
+				parent_of_nodes_in_file = node["name"]
+			elif node["parent"] == "." and parent_of_nodes_in_file != "":
+				node["parent"] = parent_of_nodes_in_file
+	
+	elif  type == "Signal":
+		for signal_var in info:
+			if signal_var["to"] == "." and parent_of_nodes_in_file != "":
+				signal_var["to"] = parent_of_nodes_in_file
+			if signal_var["from"] == "." and parent_of_nodes_in_file != "":
+				signal_var["from"] = parent_of_nodes_in_file
+	return info
